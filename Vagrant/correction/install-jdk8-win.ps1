812920b77d9dab3f53b79a7341639825fa362118
tamamaushi <koube@KoubeTomoyuki-no-Mac-mini.local>
2016-05-08 17:50:50 +0900

    clean up

--- Vagrant/correction/install-jdk8-win.ps1
+++
@@ -1,6 +0,0 @@
-
-$client = new-object System.Net.WebClient
-$client.DownloadFile("https://s3-ap-northeast-1.amazonaws.com/opendoc/jdk-8u91-windows-i586.exe", "C:\\Users\\IEUser\\Downloads\\jdk.exe")
-
-# /langは言語コード、 /sでサイレントモードで実行
-C:\Users\IEUser\Downloads\jdk.exe /s /lang=1041 
